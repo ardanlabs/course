143e33c25434ca1c8050ef874c4bda46df052f34
William Kennedy <bill@ardanlabs.com>
2021-03-19 15:39:49 -0400

    day5 commit11

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -29,7 +29,7 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, d
 		auth: a,
 	}
 	app.Handle(http.MethodGet, "/v1/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
-	// app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
+	app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
 	// app.Handle(http.MethodGet, "/v1/users/:id", ug.queryByID, mid.Authenticate(a))
 	// app.Handle(http.MethodPost, "/v1/users", ug.create, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
 	// app.Handle(http.MethodPut, "/v1/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
--- app/sales-api/handlers/user.go
+++ app/sales-api/handlers/user.go
@@ -41,3 +41,38 @@ func (ug userGroup) query(ctx context.Context, w http.ResponseWriter, r *http.Re
 
 	return web.Respond(ctx, w, users, http.StatusOK)
 }
+
+func (ug userGroup) token(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	v, ok := ctx.Value(web.KeyValues).(*web.Values)
+	if !ok {
+		return web.NewShutdownError("web value missing from context")
+	}
+
+	email, pass, ok := r.BasicAuth()
+	if !ok {
+		err := errors.New("must provide email and password in Basic auth")
+		return validate.NewRequestError(err, http.StatusUnauthorized)
+	}
+
+	claims, err := ug.user.Authenticate(ctx, v.TraceID, v.Now, email, pass)
+	if err != nil {
+		switch errors.Cause(err) {
+		case user.ErrAuthenticationFailure:
+			return validate.NewRequestError(err, http.StatusUnauthorized)
+		default:
+			return errors.Wrap(err, "authenticating")
+		}
+	}
+
+	params := web.Params(r)
+
+	var tkn struct {
+		Token string `json:"token"`
+	}
+	tkn.Token, err = ug.auth.GenerateToken(params["kid"], claims)
+	if err != nil {
+		return errors.Wrap(err, "generating token")
+	}
+
+	return web.Respond(ctx, w, tkn, http.StatusOK)
+}
